217280a0b5c0d487b76e3e72f474f6eb60a676db
unknown <nguyenngan09112000@gmail.com>
2023-07-06 17:29:35 +0700

    commit change

---
+++ src/actions/todoAction.js
@@ -0,0 +1,13 @@
+//ACTION
+export const increment = (number) => {
+    return {
+        type: "INCREMENT",
+        payload: number,
+      };
+};
+export const decrement = (number) => {
+    return {
+        type: "DECREMENT",
+        payload: number,
+      }
+};
